275983733a39291413fd2680a2f7147bdfbcafff
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-04-05 01:08:07 +0200

    Adds auth and Account.GetQuota

--- acdcli/account.go
+++ acdcli/account.go
@@ -20,17 +20,36 @@ func init() {
 	registerCommand("account", accountMain)
 }
 
+var (
+	api *acd.Client
+)
+
 func accountMain(client *http.Client, argv []string) {
 	if len(argv) != 0 {
 		fmt.Fprintln(os.Stderr, "Usage: account")
 		return
 	}
 
-	api := acd.NewClient(client)
+	api = acd.NewClient(client)
+	printAccountInfo()
+	printAccountQuota()
+}
+
+func printAccountInfo() {
 	accountInfo, _, err := api.Account.GetInfo()
 	if err != nil {
 		fmt.Printf("\nerror: %v\n\n", err)
 	}
 
-	fmt.Printf("\nTerms of use: %s\nStatus: %s\n\n", *accountInfo.TermsOfUse, *accountInfo.Status)
+	fmt.Printf("\nTerms of use: %v\nStatus: %v\n\n", *accountInfo.TermsOfUse, *accountInfo.Status)
+}
+
+func printAccountQuota() {
+	accountQuota, _, err := api.Account.GetQuota()
+	if err != nil {
+		fmt.Printf("\nerror: %v\n\n", err)
+	}
+
+	fmt.Printf("\nQuota: %v\nLast Calculated: %v\nAvailable: %v\n\n",
+		*accountQuota.Quota, *accountQuota.LastCalculated, *accountQuota.Available)
 }
