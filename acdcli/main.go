33fc3ecdb6a81f2d2395a98b8e4221c0b4081001
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-05-19 22:58:32 +0200

    Fix SemVer

--- acdcli/main.go
+++ acdcli/main.go
@@ -40,7 +40,7 @@ const (
 	AppName = "acdcli"
 
 	// The main version number that is being run at the moment.
-	Version = "0.1"
+	Version = "0.1.0"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
