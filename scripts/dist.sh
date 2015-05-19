f49d04d15b86d13c2869d983c4999007a26fbccf
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-05-19 23:33:07 +0200

    Add distribution script

---
+++ scripts/dist.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# This file was inspired by hashicorp's terraform at:
+# https://github.com/hashicorp/terraform
+
+set -e
+
+# Get the version from the command line
+VERSION=$1
+if [ -z $VERSION ]; then
+    echo "Please specify a version."
+    exit 1
+fi
+
+# Make sure we have a bintray API key
+if [ -z $BINTRAY_API_KEY ]; then
+    echo "Please set your bintray API key in the BINTRAY_API_KEY env var."
+    exit 1
+fi
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that dir because we expect that
+cd $DIR
+
+# Zip all the files
+rm -rf ./pkg/dist
+mkdir -p ./pkg/dist
+for FILENAME in $(find ./pkg -mindepth 1 -maxdepth 1 -type f); do
+    FILENAME=$(basename $FILENAME)
+    cp ./pkg/${FILENAME} ./pkg/dist/acdcli_${VERSION}_${FILENAME}
+done
+
+# Make the checksums
+pushd ./pkg/dist
+rm -f ./acdcli_${VERSION}_SHA256SUMS*
+shasum -a256 * > ./acdcli_${VERSION}_SHA256SUMS
+gpg --default-key 1FCE89F3 --detach-sig ./acdcli_${VERSION}_SHA256SUMS
+popd
+
+# Upload
+for ARCHIVE in ./pkg/dist/*; do
+    ARCHIVE_NAME=$(basename ${ARCHIVE})
+
+    echo Uploading: $ARCHIVE_NAME
+    curl \
+        -T ${ARCHIVE} \
+        -usgeb:${BINTRAY_API_KEY} \
+        "https://api.bintray.com/content/sgeb/acdcli/acdcli/${VERSION}/${ARCHIVE_NAME}"
+done
+
+exit 0
