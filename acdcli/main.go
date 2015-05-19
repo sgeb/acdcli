8e8b7ed25770b8b573cf9cf32afb78b5e344e0d3
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-05-19 23:40:03 +0200

    Prepare for release v0.1.0

--- acdcli/main.go
+++ acdcli/main.go
@@ -45,7 +45,7 @@ const (
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 )
 
 func main() {
